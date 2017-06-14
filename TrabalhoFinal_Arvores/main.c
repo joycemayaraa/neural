cc67a784f1692823b266a6a3cc2c62ef1c2b4501
Gabriel Félix <gabriel.felixramos@gmail.com>
2017-06-14 16:51:38 -0300

    [ADD

--- TrabalhoFinal_Arvores/main.c
+++ TrabalhoFinal_Arvores/main.c
@@ -2,7 +2,65 @@
 #include <stdlib.h>
 
 int main() {
-    printf("Hello word");
+    int flag = 0, escolha1 = 0, escolha2 = 0;
+    
+    while(flag == 0){
+        printf("1. Inserir novo elemento na árvore 2-3-4\n");
+        printf("2. Remover elemento da árvore 2-3-4\n");
+        printf("3. Imprimir árvore 2-3-4\n");
+        printf("4. Converter em uma árvore rubro-negra\n");
+        printf("5. Sair\n");
+        scanf("%d", &escolha1);
+        
+        switch(escolha1){
+            case 1:
+                //Função que insere elemento na 2-3-4
+                break;
+            case 2:
+                //Função que remove elemento da 2-3-4
+                break;
+            case 3:
+                //Função que imprime a arvore 2-3-4
+                break;
+            case 4:
+                //Chama a função que converte
+                system("clear");
+                flag = 1;
+                break;
+            case 5:
+                exit(1);
+                break;
+        }
+        while(flag == 1){
+            printf("1. Inserir novo elemento na árvore Rubro-Negra\n");
+            printf("2. Remover elemento da árvore Rubro-Negra\n");
+            printf("3. Imprimir árvore Rubro-Negra\n");
+            printf("4. Converter em uma árvore 2-3-4\n");
+            printf("5. Sair\n");
+            scanf("%d", &escolha2);
+        
+        switch(escolha2){
+            case 1:
+                //Função que insere elemento na rubro-negra
+                break;
+            case 2:
+                //Função que remove elemento na rubro-negra
+                break;
+            case 3:
+                //Função que imprime a rubro-negra
+                break;
+            case 4:
+                //Chama a função que converte    
+                system("clear");
+                flag = 0;
+                break;
+            case 5:
+                exit(1);
+                escolha1 = 5;
+                break;
+            }
+        }
+    }
     return 0;
 }
 
